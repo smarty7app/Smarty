7abc1b13c150a71ab3c84b4eda307ff0406deac4
smarty7app <smarty7.app@gmail.com>
2026-03-08 00:20:57 +0100

    Update AboutScreen.tsx

--- components/AboutScreen.tsx
+++ components/AboutScreen.tsx
@@ -105,7 +105,7 @@ export const AboutScreen: React.FC<AboutScreenProps> = ({ onBack }) => {
 
         {/* بصمة رقمية صغيرة (اختياري) */}
         <div className="mt-4 text-white/20 text-[8px] font-mono tracking-wider">
-          {new Date().getFullYear()} © Smartry
+          {new Date().getFullYear()} © Smatry
         </div>
       </div>
     </div>
