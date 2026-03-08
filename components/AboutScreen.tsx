5425e0f00eb03b75d4846ecfd7f9144e61017088
smarty7app <smarty7.app@gmail.com>
2026-03-08 19:34:51 +0100

    Update AboutScreen.tsx

--- components/AboutScreen.tsx
+++ components/AboutScreen.tsx
@@ -66,7 +66,7 @@ export const AboutScreen: React.FC<AboutScreenProps> = ({ onBack }) => {
             GitHub
           </a>
           <a 
-            href="https://t.me/share/url?url=https://https://smarty-lac.vercel.app/&text=جرب%20تطبيق%20Smarty%20الرائع"
+            href="https://t.me/share/url?url=https://smarty-lac.vercel.app/&text=جرب%20تطبيق%20Smarty%20الرائع"
             target="_blank" 
             rel="noopener noreferrer"
             className="flex-1 flex items-center justify-center gap-3 bg-[#0088cc] text-white py-4 rounded-2xl font-black hover:scale-[1.02] transition-transform shadow-lg"
