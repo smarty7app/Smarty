1b879980bb3876d52fe26a878fca56a48133ef07
smarty7app <smarty7.app@gmail.com>
2026-03-09 03:19:54 +0100

    Update ReminderApp.tsx

--- components/ReminderApp.tsx
+++ components/ReminderApp.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect, useRef, useMemo } from 'react';
 import { motion, AnimatePresence } from 'motion/react';
-import VoiceInput from './VoiceInput';
+import { VoiceInput } from './VoiceInput';
 import {
   Bell, 
   Plus,
