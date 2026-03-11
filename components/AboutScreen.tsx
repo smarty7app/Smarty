00953a974324d4b038ad7235498266691c182e65
smarty7app <smarty7.app@gmail.com>
2026-03-11 19:43:26 +0100

    Update AboutScreen.tsx

--- components/AboutScreen.tsx
+++ components/AboutScreen.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import { ChevronLeft, Info, Code, MessageCircle } from 'lucide-react';
 import { useLanguage } from './LanguageContext';
-
+import { motion } from "framer-motion";
 interface AboutScreenProps {
   onBack: () => void;
 }
