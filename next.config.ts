ccab12521d5f4e1be8199c6fcfe431ddf4fc92d2
smarty7app <smarty7.app@gmail.com>
2026-03-12 23:34:51 +0100

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // هذا هو السطر السحري الذي سينشئ مجلد out
-  images: {
-    unoptimized: true, // ضروري للأندرويد
-  },
-};
-
-export default nextConfig;
