6e665a87a32b7717f10e6d8f445a2e227ad8a250
smarty7app <smarty7.app@gmail.com>
2026-04-13 20:32:14 +0100

    Update error message for MONGODB_URI

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('Please define MONGODB_URI in .env.local');
+  throw new Error('Please define MONGODB_URI in environment variables');
 }
 
 const uri = process.env.MONGODB_URI;
