8fb76fdd5b5f0f37c207f315026b132eddb05d9b
smarty7app <smarty7.app@gmail.com>
2026-04-13 08:40:20 +0100

    Implement MongoDB client connection in db.ts

---
+++ lib/db.ts
@@ -0,0 +1,36 @@
+// lib/db.ts
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable');
+}
+
+const uri = process.env.MONGODB_URI;
+const options = {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+  dbName: "smartyDB", // تأكد من أن هذا هو اسم قاعدة البيانات التي أعددتها
+};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
