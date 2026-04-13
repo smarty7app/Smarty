b13a32fb352d6c5a3cbae5754fdf9de4e38f64bc
smarty7app <smarty7.app@gmail.com>
2026-04-13 05:49:57 +0100

    Change button background color in VoiceInput component

--- components/VoiceInput.tsx
+++ components/VoiceInput.tsx
@@ -13,7 +13,7 @@ export default function VoiceInput({ onTranscript }: VoiceInputProps) {
     <Link href="/smart-voice">
       <button
         type="button"
-        className="p-3 rounded-full transition-all duration-300 flex items-center justify-center bg-[#E65100] text-white hover:bg-[#E65100]/80 focus:outline-none focus:ring-2 focus:ring-white/50"
+        className="p-3 rounded-full transition-all duration-300 flex items-center justify-center bg-black text-white hover:bg-black/80 dark:bg-[#E65100] dark:hover:bg-[#E65100]/80 focus:outline-none focus:ring-2 focus:ring-white/50"
       >
         <Sparkles className="w-5 h-5" />
       </button>
