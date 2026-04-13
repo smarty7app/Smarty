9231eead8a640b63352f7ee264bbf84489261250
smarty7app <smarty7.app@gmail.com>
2026-04-13 06:01:58 +0100

    Change button styles in VoiceInput component

--- components/VoiceInput.tsx
+++ components/VoiceInput.tsx
@@ -13,7 +13,7 @@ export default function VoiceInput({ onTranscript }: VoiceInputProps) {
     <Link href="/smart-voice">
       <button
         type="button"
-        className="p-3 rounded-full transition-all duration-300 flex items-center justify-center bg-black text-white hover:bg-black/80 dark:bg-[#E65100] dark:hover:bg-[#E65100]/80 focus:outline-none focus:ring-2 focus:ring-white/50"
+        className="p-3 rounded-full transition-all duration-300 flex items-center justify-center bg-white text-[#E65100] hover:bg-gray-100 dark:bg-[#E65100] dark:text-white dark:hover:bg-[#E65100]/80 focus:outline-none focus:ring-2 focus:ring-[#E65100]/50 dark:focus:ring-white/50"
       >
         <Sparkles className="w-5 h-5" />
       </button>
