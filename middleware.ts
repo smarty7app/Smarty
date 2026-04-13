e95501cde084105873c34ac715c755b373d27ff0
smarty7app <smarty7.app@gmail.com>
2026-04-13 19:33:23 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,19 @@
 // middleware.ts
-import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
- 
-export default NextAuth(authConfig).auth;
- 
+import { auth } from "./auth";
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  const isOnProtectedRoute = req.nextUrl.pathname === "/" || req.nextUrl.pathname.startsWith("/smart-voice");
+  
+  if (isOnProtectedRoute && !isLoggedIn) {
+    const loginUrl = new URL("/login", req.url);
+    return NextResponse.redirect(loginUrl);
+  }
+  
+  return NextResponse.next();
+});
+
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
