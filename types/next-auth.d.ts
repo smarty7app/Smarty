ffa7834364bd8301fbb095e15be362003c6e64b8
smarty7app <smarty7.app@gmail.com>
2026-04-14 06:00:58 +0100

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,18 +1,20 @@
 // types/next-auth.d.ts
-import "next-auth";
+import { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      email: string;
-      name: string;
-    }
+    } & DefaultSession["user"];
   }
-  
-  interface User {
+
+  interface User extends DefaultUser {
     id: string;
-    email: string;
-    name: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
   }
 }
