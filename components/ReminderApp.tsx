c6d46a500380a8d5c310cb7777ae10f5917ae652
smarty7app <smarty7.app@gmail.com>
2026-04-18 02:37:40 +0000

    Fix API route params type for Next.js 15 and remove unused eslint disables

--- components/ReminderApp.tsx
+++ components/ReminderApp.tsx
@@ -82,9 +82,7 @@ export default function ReminderApp({ initialReminderText }: { initialReminderTe
    
   useEffect(() => {
     if (initialReminderText) {
-      // eslint-disable-next-line react-hooks/set-state-in-effect
       setInputText(initialReminderText);
-      // eslint-disable-next-line react-hooks/set-state-in-effect
       setIsAdding(true);
     }
   }, [initialReminderText]);
