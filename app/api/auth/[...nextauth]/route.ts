2997003667a7c48a8c0d2e5453705a3957601ef2
smarty7app <smarty7.app@gmail.com>
2026-04-26 00:59:04 +0100

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth"; // استيراد من الملف الجديد
 import GoogleProvider from "next-auth/providers/google";
 import clientPromise from "@/lib/mongodb";
 
